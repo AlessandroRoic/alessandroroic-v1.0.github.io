8889b10e42adbcda50f4f4418722f3f55e6dd598
AlessandroRoic <29387855+AlessandroRoic@users.noreply.github.com>
2021-11-21 17:39:16 +0100

    feat: adds card component

---
+++ src/app/components/shared/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export default class CardComponent {}
